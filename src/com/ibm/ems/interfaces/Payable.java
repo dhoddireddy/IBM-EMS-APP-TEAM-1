2a56fb567af2d45dcf342f1e1cfbcdf7863c7426
Poorvika B M <poorvika.b.m@ibm.com>
2026-05-06 13:37:23 +0530

    Payable Java Modification

--- src/com/ibm/ems/interfaces/Payable.java
+++ src/com/ibm/ems/interfaces/Payable.java
@@ -2,4 +2,15 @@ package com.ibm.ems.interfaces;
 
 public interface Payable {
     double calculateSalary();
+
+
+
+//   // Other methods related to payment processing can be added here
+//   For example:
+//   void processPayment(); 
+//   void generatePaySlip();
+//  These methods can be implemented by the classes that implement this interface
+
+
 }
+
