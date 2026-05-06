7e8bf5035efb59483488d0d406601a8e23327f3e
govardhan-majjiga <govam8444@gmail.com>
2026-05-06 14:17:20 +0530

    updated salary logic and navbar changes

--- src/com/ibm/ems/model/ContractEmployee.java
+++ src/com/ibm/ems/model/ContractEmployee.java
@@ -5,11 +5,11 @@ public class ContractEmployee extends Employee {
     public ContractEmployee(int id, String name, double salary) {
         super(id, name, salary);
     }
+private double deductionRate = 0.05;// 5% deduction for contract employees  
 
-    @Override
-    public double calculateSalary() {
-        double salary = getSalary();
-        double deduction = salary * 0.05; // 5% deduction
-        return salary - deduction;
-    }
+@Override
+public double calculateSalary() {
+    double salary = getSalary();
+    double deduction = salary * deductionRate;
+    return salary - deduction;
 }
